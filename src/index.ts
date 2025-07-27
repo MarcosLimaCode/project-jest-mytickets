d12c072c866e57c72297c5ba20dbe60d11294c6f
Marcos Lima <mrsk1414@gmail.com>
2025-07-27 14:44:20 -0300

    feat: supertest config

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,11 @@ dotenv.config();
 const app = express();
 app.use(json());
 
-app.get("/health", (req: Request, res: Response) => res.status(httpStatus.OK).send(`I'm okay!`));
+app.get("/health", (req: Request, res: Response) =>
+  res.status(httpStatus.OK).send(`I'm okay!`)
+);
 app.use(ticketsRouter);
 app.use(eventsRouter);
 app.use(errorHandlerMiddleware);
 
-const port = +process.env.PORT || 5000;
-app.listen(port, () => console.log("Server is up and running on port " + port));
+export default app;
--- tests/example.test.ts
+++
@@ -1,5 +0,0 @@
-describe("agrupamento de testes", () => {
-  it("2+2 deve ser 4", () => {
-    expect(2 + 2).toBe(4);
-  });
-});
