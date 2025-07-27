b40166e51bd77701bca6520b90c29e11c4af7361
Marcos Lima <mrsk1414@gmail.com>
2025-07-27 20:48:28 -0300

    feat: create put and delete tests on events file

--- tests/events.test.ts
+++ tests/events.test.ts
@@ -1,7 +1,11 @@
 import prisma from "database";
 import app from "../src/index";
 import supertest from "supertest";
-import { faker } from "@faker-js/faker";
+import {
+  createEvent,
+  postEvent,
+  postWrongEvent,
+} from "./factories/events-factory";
 
 const api = supertest(app);
 
@@ -11,17 +15,9 @@ beforeEach(async () => {
 
 describe("POST /event", () => {
   it("should a create event", async () => {
-    //a1
-    const { status } = await api.post("/events").send({
-      name: faker.lorem.words(2),
-      date: faker.date.future(),
-    });
-
-    //a2
+    const data = await postEvent();
+    const { status } = await api.post("/events").send(data);
     const { body } = await api.get("/events");
-
-    //a3
-
     expect(status).toBe(201);
     expect(body).toHaveLength(1);
     expect(body).toEqual(
@@ -43,36 +39,12 @@ describe("GET /event", () => {
   });
 
   it("should return all events", async () => {
-    //a1
-
-    await prisma.event.create({
-      data: {
-        name: faker.lorem.words(2),
-        date: faker.date.future(),
-      },
-    });
-
-    await prisma.event.create({
-      data: {
-        name: faker.lorem.words(2),
-        date: faker.date.future(),
-      },
-    });
-
-    await prisma.event.create({
-      data: {
-        name: faker.lorem.words(2),
-        date: faker.date.future(),
-      },
-    });
-
-    //a2
+    await createEvent();
+    await createEvent();
+    await createEvent();
     const { status, body } = await api.get("/events");
-
-    //a3
     expect(status).toBe(200);
     expect(body).toHaveLength(3);
-
     expect(body).toEqual(
       expect.arrayContaining([
         expect.objectContaining({
@@ -85,16 +57,62 @@ describe("GET /event", () => {
   });
 
   it("should return especific event by id", async () => {
-    const { id } = await prisma.event.create({
-      data: {
-        name: faker.lorem.words(2),
-        date: faker.date.future(),
-      },
-    });
+    const { id } = await createEvent();
     const { status, body } = await api.get(`/events/${id}`);
     expect(status).toBe(200);
     expect(body).toMatchObject({
       id: id,
     });
+  });
+
+  it("should return error at wrong id", async () => {
+    const { status } = await api.get("/events/9999");
+    expect(status).toBe(404);
+  });
+
+  it("should return error at invalid id", async () => {
+    const { status } = await api.get("/events/9999n");
+    expect(status).toBe(400);
+  });
+
+  it("should return error at wrong format", async () => {
+    const data = await postWrongEvent();
+    const { status } = await api.post("/events").send(data);
+    expect(status).toBe(422);
+  });
+
+  it("should return error at duplicate event", async () => {
+    const data = await postEvent();
+    await api.post("/events").send(data);
+    const { status } = await api.post("/events").send(data);
+    expect(status).toBe(409);
+  });
+});
+
+describe("PUT /event", () => {
+  it("should change a event", async () => {
+    const { id } = await createEvent();
+    const data = await postEvent();
+    const { status } = await api.put(`/events/${id}`).send(data);
+    const { body } = await api.get(`/events/${id}`);
+    expect(status).toBe(200);
+    expect(body).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        name: expect.any(String),
+        date: expect.any(String),
+      })
+    );
+  });
+});
+
+describe("DELETE /event", () => {
+  it("should delete a event", async () => {
+    const { id } = await createEvent();
+
+    const { status } = await api.delete(`/events/${id}`);
+    const result = await api.get(`/events/${id}`);
+    expect(status).toBe(204);
+    expect(result).toEqual(expect.arrayContaining([]));
   });
 });
